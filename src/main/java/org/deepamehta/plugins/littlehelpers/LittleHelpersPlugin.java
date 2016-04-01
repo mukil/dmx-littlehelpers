486c08479d121a63e9a31c602b99dc4fb8fa99bc
Malte Reißig <malte@mikromedia.de>
2016-04-01 20:38:09 +0200

    Integrated timerange helper methods from eduzen-gui.

--- src/main/java/org/deepamehta/plugins/littlehelpers/LittleHelpersPlugin.java
+++ src/main/java/org/deepamehta/plugins/littlehelpers/LittleHelpersPlugin.java
@@ -12,21 +12,27 @@ import javax.ws.rs.Consumes;
 
 import de.deepamehta.core.Topic;
 import de.deepamehta.core.TopicType;
+import de.deepamehta.core.model.ChildTopicsModel;
 
 import de.deepamehta.core.osgi.PluginActivator;
 import de.deepamehta.core.service.Inject;
+import de.deepamehta.plugins.time.TimeService;
 import de.deepamehta.plugins.workspaces.WorkspacesService;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
 import javax.ws.rs.core.MediaType;
+import org.codehaus.jettison.json.JSONArray;
 
 
 /**
  * @author Malte Reißig (<malte@mikromedia.de>)
  * @website http://github.com/mukil/dm4-littlehelpers
- * @version 0.1.0 - compatible with DeepaMehta 4.5
+ * @version 0.2 - compatible with DM 4.7
  *
  */
-
 @Path("/helpers")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
@@ -36,12 +42,22 @@ public class LittleHelpersPlugin extends PluginActivator implements LittleHelper
 
     // --- DeepaMehta Standard URIs
 
+    private final static String PROP_URI_CREATED  = "dm4.time.created";
+    private final static String PROP_URI_MODIFIED = "dm4.time.modified";
+
     /** private final static String CHILD_URI = "dm4.core.child";
     private final static String PARENT_URI = "dm4.core.parent";
     private final static String AGGREGATION = "dm4.core.aggregation"; **/
 
     @Inject WorkspacesService wsService;
-    
+    @Inject TimeService timeService;
+
+
+
+    // --
+    // --- Stableviews Utility Service
+    // --
+
     @GET
     @Override
     @Path("/suggest/topics/{input}")
@@ -78,10 +94,173 @@ public class LittleHelpersPlugin extends PluginActivator implements LittleHelper
         return dms.searchTopics(query + "*", typeUri);
     }
 
+
+
     // --
-    // --- Helper Methods taken from the WebclientPlugin.java by Jörg Richter
+    // --- Timeline Utility Service (Formerly eduZEN Notizen)
     // --
 
+    /**
+     * Fetches standard topics by time-range and time-value (created || modified).
+     */
+    @GET
+    @Path("/by_time/{time_value}/{from}/{to}")
+    @Produces("application/json")
+    public String getStandardTopicsInTimeRange(@PathParam("time_value") String type, @PathParam("from") long from, @PathParam("to") long to) {
+        JSONArray results = new JSONArray();
+        try {
+            // 1) Fetch all topics in either "created" or "modified"-timestamp timerange
+            log.info("Fetching Topics in timerange from \"" + from + "\" to \"" + to + "\"");
+            ArrayList<Topic> items_in_range = new ArrayList<Topic>(); // items of interest
+            Collection<Topic> topics_in_range = null; // all topics
+            if (type.equals("created")) {
+                topics_in_range = timeService.getTopicsByCreationTime(from, to);
+            } else if (type.equals("modified")) {
+                topics_in_range = timeService.getTopicsByModificationTime(from, to);
+            } else {
+                return "Wrong parameter: set time_value to \"created\" or \"modified\"";
+            }
+            if (topics_in_range.isEmpty()) log.info("getStandardTopicsInTimeRange("+type+") got NO result.");
+            Iterator<Topic> resultset = topics_in_range.iterator();
+            while (resultset.hasNext()) {
+                Topic in_question = resultset.next();
+                if (in_question.getTypeUri().equals("dm4.notes.note")) {
+                    items_in_range.add(in_question);
+                } else if (in_question.getTypeUri().equals("dm4.files.file")) {
+                    items_in_range.add(in_question);
+                } else if (in_question.getTypeUri().equals("dm4.files.folder")) {
+                    items_in_range.add(in_question);
+                } else if (in_question.getTypeUri().equals("dm4.contacts.person")) {
+                    items_in_range.add(in_question);
+                } else if (in_question.getTypeUri().equals("dm4.contacts.institution")) {
+                    items_in_range.add(in_question);
+                } else if (in_question.getTypeUri().equals("dm4.webbrowser.web_resource")) {
+                    items_in_range.add(in_question);
+                } else {
+                    // log.info("> Result \"" +in_question.getSimpleValue()+ "\" (" +in_question.getTypeUri()+ ")");
+                }
+            }
+            log.info("> Fetched " +items_in_range.size()+ " elements (" + from + ", " + to + ")" + " by time");
+            // 2) Sort all fetched items by their "created" or "modified" timestamps
+            ArrayList<Topic> in_memory_resources = null;
+            if (type.equals("created")) {
+                in_memory_resources = getTopicListSortedByCreationTime(items_in_range);
+            } else if (type.equals("modified")) {
+                in_memory_resources = getTopicListSortedByModificationTime(items_in_range);
+            }
+            // 3) Prepare the notes page-results view-model (per type of interest)
+            for (Topic item : in_memory_resources) {
+                try {
+                    item.loadChildTopics();
+                    enrichTopicModelAboutCreationTimestamp(item);
+                    enrichTopicModelAboutModificationTimestamp(item);
+                    results.put(item.toJSON());
+                } catch (RuntimeException rex) {
+                    log.warning("Could not add fetched item to results, caused by: " + rex.getMessage());
+                }
+            }
+        } catch (Exception e) { // e.g. a "RuntimeException" is thrown if the moodle-plugin is not installed
+            throw new RuntimeException("something went wrong", e);
+        }
+        return results.toString();
+    }
+
+    /**
+     * Getting composites of all standard topics in given timerange.
+     */
+    @GET
+    @Path("/all/index/{from}/{to}")
+    @Produces("application/json")
+    public String getStandardIndexForTimeRange(@PathParam("from") long from, @PathParam("to") long to) {
+        //
+        JSONArray results = new JSONArray();
+        try {
+            // 1) Fetch Resultset of Resources
+            log.info("Loadings all standard topic composites in timerange from \"" + from + "\" to \"" + to + "\"");
+            ArrayList<Topic> all_in_range = new ArrayList<Topic>();
+            Collection<Topic> topics_in_range = timeService.getTopicsByCreationTime(from, to);
+            Iterator<Topic> resultset = topics_in_range.iterator();
+            while (resultset.hasNext()) {
+                Topic in_question = resultset.next();
+                if (in_question.getTypeUri().equals("dm4.notes.note") ||
+                    in_question.getTypeUri().equals("dm4.files.file") ||
+                    in_question.getTypeUri().equals("dm4.files.folder") ||
+                    in_question.getTypeUri().equals("dm4.contacts.person") ||
+                    in_question.getTypeUri().equals("dm4.contacts.institution") ||
+                    in_question.getTypeUri().equals("dm4.webbrowser.web_resource")) {
+                    // log.info("> " +in_question.getSimpleValue()+ " of type \"" +in_question.getTypeUri()+ "\"");
+                    all_in_range.add(in_question);
+                }
+            }
+            log.info("> Fetched  " +all_in_range.size()+ " items (" + from + ", " + to + ")" + " by time");
+            // 2) Sort and fetch resources
+            // ArrayList<RelatedTopic> in_memory_resources = getResultSetSortedByCreationTime(all_resources);
+            for (Topic item : all_in_range) { // 2) prepare resource items
+                // 3) Prepare the notes page-results view-model
+                item.loadChildTopics();
+                enrichTopicModelAboutCreationTimestamp(item);
+                enrichTopicModelAboutModificationTimestamp(item);
+                results.put(item.toJSON());
+            }
+        } catch (Exception e) { // e.g. a "RuntimeException" is thrown if the moodle-plugin is not installed
+            throw new RuntimeException("something went wrong", e);
+        }
+        return results.toString();
+    }
+
+    public ArrayList<Topic> getTopicListSortedByCreationTime (ArrayList<Topic> all) {
+        Collections.sort(all, new Comparator<Topic>() {
+            public int compare(Topic t1, Topic t2) {
+                try {
+                    Object one = t1.getProperty(PROP_URI_CREATED);
+                    Object two = t2.getProperty(PROP_URI_CREATED);
+                    if ( Long.parseLong(one.toString()) < Long.parseLong(two.toString()) ) return 1;
+                    if ( Long.parseLong(one.toString()) > Long.parseLong(two.toString()) ) return -1;
+                } catch (Exception nfe) {
+                    log.warning("Error while accessing timestamp of Topic 1: " + t1.getId() + " Topic2: "
+                            + t2.getId() + " nfe: " + nfe.getMessage());
+                    return 0;
+                }
+                return 0;
+            }
+        });
+        return all;
+    }
+
+    public ArrayList<Topic> getTopicListSortedByModificationTime (ArrayList<Topic> all) {
+        Collections.sort(all, new Comparator<Topic>() {
+            public int compare(Topic t1, Topic t2) {
+                try {
+                    Object one = t1.getProperty(PROP_URI_MODIFIED);
+                    Object two = t2.getProperty(PROP_URI_MODIFIED);
+                    if ( Long.parseLong(one.toString()) < Long.parseLong(two.toString()) ) return 1;
+                    if ( Long.parseLong(one.toString()) > Long.parseLong(two.toString()) ) return -1;
+                } catch (Exception nfe) {
+                    log.warning("Error while accessing timestamp of Topic 1: " + t1.getId() + " Topic2: "
+                            + t2.getId() + " nfe: " + nfe.getMessage());
+                    return 0;
+                }
+                return 0;
+            }
+        });
+        return all;
+    }
+
+    // --- Private Utility Methods
+
+    private void enrichTopicModelAboutCreationTimestamp (Topic resource) {
+        long created = timeService.getCreationTime(resource.getId());
+        ChildTopicsModel resourceModel = resource.getChildTopics().getModel();
+        resourceModel.put(PROP_URI_CREATED, created);
+    }
+
+    private void enrichTopicModelAboutModificationTimestamp (Topic resource) {
+        long created = timeService.getModificationTime(resource.getId());
+        ChildTopicsModel resourceModel = resource.getChildTopics().getModel();
+        resourceModel.put(PROP_URI_MODIFIED, created);
+    }
+
+    /** Taken from the WebclientPlugin.java by Jörg Richter */
     private List<Topic> findSearchableUnits(List<? extends Topic> topics) {
         List<Topic> searchableUnits = new ArrayList<Topic>();
         for (Topic topic : topics) {
--- src/main/java/org/deepamehta/plugins/littlehelpers/LittleHelpersService.java
+++ src/main/java/org/deepamehta/plugins/littlehelpers/LittleHelpersService.java
@@ -2,22 +2,28 @@ package org.deepamehta.plugins.littlehelpers;
 
 
 import de.deepamehta.core.Topic;
+import java.util.ArrayList;
 import java.util.List;
-import org.deepamehta.plugins.littlehelpers.SuggestionViewModel;
 
 /**
  * A plugin-service helping with DeepaMehta 4 plugin development.
  *
  * @author Malte Reißig (<malte@mikromedia.de>)
- * @website http://github.com/mukil/dm4-helpers
- * @version 0.1.0 - compatible with DM 4.5
- *
+ * @website http://github.com/mukil/dm4-littlehelpers
+ * @version 0.2 - compatible with DM 4.7
  */
-
 public interface LittleHelpersService {
 
     List<SuggestionViewModel> getTopicSuggestions(String query);
-    
+
     List<Topic> getTopicSuggestions(String query, String typeUri);
+
+    ArrayList<Topic> getTopicListSortedByCreationTime(ArrayList<Topic> list);
+
+    ArrayList<Topic> getTopicListSortedByModificationTime(ArrayList<Topic> list);
+
+    String getStandardTopicsInTimeRange(String type, long from, long to);
+
+    String getStandardIndexForTimeRange(long from, long to);
 
 }
