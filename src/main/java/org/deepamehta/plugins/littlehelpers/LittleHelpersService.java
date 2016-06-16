f82db207bf2c16bbac30fa842ffe37323ee2d9ff
Malte Reißig <malte@mikromedia.de>
2016-06-16 18:04:51 +0200

    Adapted to be compatible with Dm 4.8

--- src/main/java/org/deepamehta/plugins/littlehelpers/LittleHelpersService.java
+++ src/main/java/org/deepamehta/plugins/littlehelpers/LittleHelpersService.java
@@ -2,7 +2,6 @@ package org.deepamehta.plugins.littlehelpers;
 
 
 import de.deepamehta.core.Topic;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -10,7 +9,7 @@ import java.util.List;
  *
  * @author Malte Reißig (<malte@mikromedia.de>)
  * @website http://github.com/mukil/dm4-littlehelpers
- * @version 0.2 - compatible with DM 4.7
+ * @version 0.3 - compatible with DM 4.8
  */
 public interface LittleHelpersService {
 
@@ -18,9 +17,13 @@ public interface LittleHelpersService {
 
     List<Topic> getTopicSuggestions(String query, String typeUri);
 
-    ArrayList<Topic> getTopicListSortedByCreationTime(ArrayList<Topic> list);
+    List<? extends Topic> getTopicListSortedByCreationTime(List<? extends Topic> list);
 
-    ArrayList<Topic> getTopicListSortedByModificationTime(ArrayList<Topic> list);
+    List<? extends Topic> getTopicListSortedByModificationTime(List<? extends Topic> list);
+
+    void sortAlphabeticalDescendingByChild(List<? extends Topic> topics, final String childTypeUri);
+
+    List<? extends Topic> sortAlphabeticalDescending(List<? extends Topic> topics);
 
     String getStandardTopicsInTimeRange(String modifiedOrCreated, long from, long to);
 
